e456b69e7521fc0593aabd9384c470a9e69b196f
AlexanderVin <alexandr.vinitsky@gmail.com>
2021-06-02 22:28:49 +0300

    add 02 06

--- bookmarks/bboard/views.py
+++ bookmarks/bboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+from .models import import Bb
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    model = Bb
